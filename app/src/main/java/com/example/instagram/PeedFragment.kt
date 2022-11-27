54a197977db5ca9f5ecdde9aa3f23478c13cc422
junhyun1001 <jjunhyun1001@naver.com>
2022-11-27 14:05:51 +0900

    22-11-27 JunHyun 이미지 2번 클릭시 좋아요 기능 추가

--- app/src/main/java/com/example/instagram/PeedFragment.kt
+++ app/src/main/java/com/example/instagram/PeedFragment.kt
@@ -27,6 +27,8 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
     var uid: String? = null
     var user: FirebaseUser? = null
 
+    var image: ImageView? = null
+
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).showNav()
@@ -34,6 +36,7 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
         firestore = FirebaseFirestore.getInstance()
         uid = FirebaseAuth.getInstance().currentUser?.uid
         user = FirebaseAuth.getInstance().currentUser
+
 
         // 리사이클러 뷰를 사용하려면 두 가지가 필수적으로 필요하다. Adpater와 LayoutManager이다.
         binding.detailviewfragmentRecyclerview.adapter = PeedAdapter()
@@ -50,6 +53,8 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
 
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
         var contentUidList: ArrayList<String> = arrayListOf()
+
+        var delay: Long = 0
 
         init {
             firestore = FirebaseFirestore.getInstance()
@@ -127,8 +132,8 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
 //                        setFragmentResult("userId", bundleOf("uid" to uid))
                         println("###############   destinationUid: ${contentDTOs[position].uid}")
                         println("###############   userId: ${contentDTOs[position].userId}")
-                        setFragmentResult("destinationUid", bundleOf("uid" to uid))
-                        setFragmentResult("userId", bundleOf("DTOsUserId" to contentDTOs[position].userId))
+//                        setFragmentResult("destinationUid", bundleOf("uid" to uid))
+//                        setFragmentResult("userId", bundleOf("DTOsUserId" to contentDTOs[position].userId))
 
                         findNavController().navigate(R.id.action_peedFragment_to_myProfileFragment)
                     } else {
@@ -136,10 +141,20 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
                         println("@@@@@@@@@@@@@@@   destinationUid: ${contentDTOs[position].uid}")
                         println("@@@@@@@@@@@@@@@   userId: ${contentDTOs[position].userId}")
                         setFragmentResult("destinationUid", bundleOf("DTOsUid" to contentDTOs[position].uid))
-                        setFragmentResult("userId", bundleOf("DTOsUserId" to contentDTOs[position].userId))
+//                        setFragmentResult("userId", bundleOf("DTOsUserId" to contentDTOs[position].userId))
                         findNavController().navigate(R.id.action_peedFragment_to_yourProfileFragment)
                     }
                 }
+
+            viewHolder.findViewById<ImageView>(R.id.detailviewitem_imageview_content).setOnClickListener {
+                if(System.currentTimeMillis() > delay){
+                    delay = System.currentTimeMillis() + 200
+                    return@setOnClickListener
+                }
+                if(System.currentTimeMillis() <= delay) {
+                    favoriteEvent(position)
+                }
+            }
 
             // 유저 아이디
             viewHolder.findViewById<TextView>(R.id.detailviewitem_profile_textview).text =
@@ -156,6 +171,7 @@ class PeedFragment : BaseFragment<FragmentPeedBinding>(R.layout.fragment_peed) {
             // 좋아요 이벤트
             viewHolder.findViewById<ImageView>(R.id.detailviewitem_favorite_imageview)
                 .setOnClickListener { favoriteEvent(position) }
+
 
             //좋아요 버튼 설정
             if (contentDTOs[position].favorites.containsKey(FirebaseAuth.getInstance().currentUser!!.uid)) {
